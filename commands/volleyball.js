2765128d88f71b1a882d9e2bc0ca23f72d8ece72
Ata Atasoy <h.ataatasoy@gmail.com>
2021-11-18 22:53:48 +0300

    Add volleyball command

---
+++ commands/volleyball.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const dotenv = require('dotenv');
+const http = require('http');
+dotenv.config();
+
+const SCRAPER_API_URL = process.env.SCRAPER_URL;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('volleyball')
+        .setDescription('Display the tickets that are on sale for the upcoming volleyball matches.'),
+    async execute(interaction) {
+        const data;
+        const options = {
+            hostname: SCRAPER_API_URL,
+            path: "/volleyball",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        const request = http.get(options, (req, res) => {
+            console.log(res.data)
+            data = res.data;
+        })
+
+        return interaction.reply(`Volleyball Matches: ${data}\n`);
+    },
+};
